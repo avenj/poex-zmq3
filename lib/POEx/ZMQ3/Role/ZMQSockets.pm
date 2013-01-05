960f705dfa0d4c2cd08afa7fb1e925301b04f6d5
Jon Portnoy <avenj@cobaltirc.org>
2013-01-05 00:08:36 -0500

    POD
    	modified:   lib/POEx/ZMQ3/Role/ZMQSockets.pm

--- lib/POEx/ZMQ3/Role/ZMQSockets.pm
+++ lib/POEx/ZMQ3/Role/ZMQSockets.pm
@@ -77,7 +77,7 @@ sub create_zmq_socket {
   confess "Expected an alias and ZMQ::Constants socket type constant"
     unless defined $alias and defined $type;
 
-  $self->_create_zmq_socket_sess;
+  my $sess_id = $self->_create_zmq_socket_sess;
 
   confess "Alias $alias exists; clear it first"
     if $self->get_zmq_socket($alias);
@@ -94,7 +94,7 @@ sub create_zmq_socket {
     fd     => $fd,
   };
 
-  $poe_kernel->call( $self->_zmq_zsock_sess, 
+  $poe_kernel->call( $sess_id,
     'zsock_handle_socket',
     $alias
   );
@@ -236,3 +236,161 @@ sub _zsock_start { 1 }
 
 
 1;
+
+=pod
+
+=head1 NAME
+
+POEx::ZMQ3::Role::ZMQSockets - Add ZeroMQ sockets to a class
+
+=head1 SYNOPSIS
+
+  ## A 'REP' (reply) server that pongs mindlessly, given a ping.
+  ## (Call ->start() from a POE-enabled class/app.)
+  package MyZMQServer;
+  use Moo;
+  use ZMQ::Constants ':all';
+
+  with 'POEx::ZMQ3::Role::ZMQSockets';
+
+  sub start {
+    my ($self) = @_;
+    $self->create_zmq_socket( 'my_server', ZMQ_REP );
+    $self->bind_zmq_socket( 'my_server', "tcp://127.0.0.1:$port" );
+  }
+
+  sub stop {
+    my ($self) = @_;
+    $self->clear_all_zmq_sockets;
+  }
+
+  sub zmq_message_ready {
+    my ($self, $zsock_alias, $zmq_msg, $raw_data) = @_;
+    $self->write_zmq_socket( $zsock_alias, "PONG!" )
+      if $raw_data =~ /^PING/i;
+  }
+
+=head1 DESCRIPTION
+
+A L<Moo::Role> giving its consuming class L<POE>-enabled asynchronous
+B<ZeroMQ> sockets via L<ZMQ::LibZMQ3>.
+
+See L<http://www.zeromq.org> for more about ZeroMQ.
+
+=head2 Overrides
+
+These methods should be overriden in your consuming class:
+
+=head3 zmq_message_ready
+
+  sub zmq_message_ready {
+    my ($self, $zsock_alias, $zmq_msg, $raw_data) = @_;
+    . . .
+  }
+
+Required.
+
+The B<zmq_message_ready> method should be defined in the consuming class to
+handle a received message.
+
+Arguments are the ZMQ socket's alias, the L<ZMQ::LibZMQ3> message object, 
+and the raw data retrieved from the message object, respectively.
+
+=head3 zmq_socket_cleared
+
+  sub zmq_socket_cleared {
+    my ($self, $zsock_alias) = @_;
+    . . .
+  }
+
+Optional.
+
+Indicates a ZMQ socket has been cleared.
+
+
+=head2 Attributes
+
+=head3 context
+
+The B<context> attribute is the ZeroMQ context object as created by
+L<ZMQ::LibZMQ3/"zmq_init">.
+
+These objects can be shared, so long as they are reset/reconstructed 
+in any forked copies.
+
+
+=head2 Methods
+
+=head3 create_zmq_socket
+
+  my $zsock = $self->create_zmq_socket( $zsock_alias, $zsock_type_constant );
+
+Creates (and begins watching) a ZeroMQ socket.
+Expects an (arbitrary) alias and a valid L<ZMQ::Constants> socket type constant.
+See the man page for B<zmq_socket> for details.
+
+If a L<POE::Session> to manage ZMQ sockets did not previously exist, one is
+spawned when B<create_zmq_socket> is called.
+
+=head3 bind_zmq_socket
+
+  $self->bind_zmq_socket( $zsock_alias, $endpoint );
+
+Binds a "listening" socket type to a specified endpoint.
+
+For example:
+
+  $self->bind_zmq_socket( 'my_serv', 'tcp://127.0.0.1:5552' );
+
+See the man pages for B<zmq_bind> and B<zmq_connect> for details.
+
+=head3 connect_zmq_socket
+
+  $self->connect_zmq_socket( $zsock_alias, $target );
+
+Connects a "client" socket type to a specified target endpoint.
+
+See the man pages for B<zmq_connect> and B<zmq_bind> for details.
+
+Note that ZeroMQ manages its own actual connections; 
+a successful call to B<zmq_connect> does not necessarily mean a 
+persistent connection is open. See the ZeroMQ documentation for details.
+
+=head3 clear_zmq_socket
+
+  $self->clear_zmq_socket( $zsock_alias );
+
+Shut down a specified socket.
+
+=head3 clear_all_zmq_sockets
+
+  $self->clear_all_zmq_sockets;
+
+Shut down all sockets.
+
+=head3 get_zmq_socket
+
+  my $zsock = $self->get_zmq_socket( $zsock_alias );
+
+Retrieve the actual ZeroMQ socket object for the given alias.
+
+Only useful for darker forms of magic.
+
+=head3 write_zmq_socket
+
+  $self->write_zmq_socket( $zsock_alias, $data );
+
+Write raw data or a ZeroMQ message object to the specified socket alias.
+
+
+=head1 SEE ALSO
+
+L<ZMQ::LibZMQ3>
+
+L<http://www.zeromq.org>
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+=cut
